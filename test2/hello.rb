6b74063175afeb96ea0031c7c2d6bc7b1d4628b7
TakuyaMurasawa <bandage104.batt1e37@gmail.com>
2024-04-23 19:48:24 +0900

    [NEW/UPDATE]test2/hello.rb

---
+++ test2/hello.rb
@@ -0,0 +1,9 @@
+puts "Hello, world!"
+puts "qauity!".length
+puts "qauity".reverse
+puts "hfeekfnqauitywje".include?("qauity")
+puts "hfeekfnqauitwje".include?("qauity")
+test = "query"
+puts test
+test = "quarity"
+puts test
