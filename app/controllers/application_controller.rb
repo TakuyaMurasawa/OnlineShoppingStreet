69f5a06dc8215b4dce5f9e860c0cc84069b489c2
TakuyaMurasawa <bandage104.batt1e37@gmail.com>
2024-04-28 23:00:23 +0900

    [UPDATE]devise/view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  # devise利用の機能（ユーザ登録、ログイン認証など）が使われる時に下記のconfigure_permitted_parametersを実行
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana, :address, :birthdate,:phone_number])
 end
