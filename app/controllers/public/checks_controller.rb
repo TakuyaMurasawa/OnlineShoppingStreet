b64249e4e65084ce47102e04ffb1573abda40d96
TakuyaMurasawa <bandage104.batt1e37@gmail.com>
2024-05-06 12:04:30 +0900

    [UPDATE]controller/public/checks

--- app/controllers/public/checks_controller.rb
+++ app/controllers/public/checks_controller.rb
@@ -5,10 +5,10 @@ class Public::ChecksController < ApplicationController
 
   def reject_customer
     @customer = Customer.find_by(email: params[:customer][:email].downcase)
-    //ログイン時に入力されたemailが存在するか探す。
+    # //ログイン時に入力されたemailが存在するか探す。
     if @customer
       if (@customer.valid_password?(params[:customer][:password]) && (@customer.active_for_authentication? == false))
-    //入力されたパスワードが正しいこと 且つ　active_for_authentication?メソッドがfalseであるかどうか。
+    # //入力されたパスワードが正しいこと 且つ　active_for_authentication?メソッドがfalseであるかどうか。
         flash[:alert] = "このアカウントは退会済みです。"
         redirect_to new_customer_session_path
       end
