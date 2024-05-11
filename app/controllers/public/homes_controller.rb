825f595a7836ca1438d6db14da396c891542aa57
TakuyaMurasawa <bandage104.batt1e37@gmail.com>
2024-05-11 11:31:52 +0900

    [UPDATE]controller/public/homes

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,2 +1,9 @@
 class Public::HomesController < ApplicationController
+  def top
+    @genres = Genre.all
+    @items = Item.where(is_active: true).page(params[:page]).per(4).reverse_order
+  end
+
+  def about
+  end
 end
