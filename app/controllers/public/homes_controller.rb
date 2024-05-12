105db41abbfd441d129a5f784f279ec8f095811c
TakuyaMurasawa <bandage104.batt1e37@gmail.com>
2024-05-12 23:00:33 +0900

    [UPDATE]controller/public/homes

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
     @genres = Genre.all
+    # 商品一覧を4ページにまとめる
     @items = Item.where(is_active: true).page(params[:page]).per(4).reverse_order
   end
 
